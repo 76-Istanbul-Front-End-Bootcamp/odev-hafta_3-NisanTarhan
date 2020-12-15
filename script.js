6c887bedfa3bc72092c89d8ecc26124d26a28fb2
Nisan Tarhan <nisan_tarhan@hotmail.com>
2020-12-15 19:26:59 +0300

    prototypyal inheritance pattern converted to class

--- script.js
+++ script.js
@@ -1,52 +1,102 @@
-function Animal(name){
-    this.name = name;
+// function Animal(name) {
+//     this.name = name;
+// }
+
+// Animal.prototype.action = function () {
+//     document.getElementById(this.actionSoundName).play();
+
+// }
+
+// Animal.prototype.putInTheDocument = function () {
+// var petsTable = document.getElementById("petsTable");
+// var petTR = document.createElement("tr");
+
+// var petNameTD = document.createElement("td");
+// petNameTD.textContent = this.name;
+// petTR.appendChild(petNameTD);
+
+// var petLegsTD = document.createElement("td");
+// petLegsTD.textContent = this.legs;
+// petTR.appendChild(petLegsTD);
+
+// var petActionTD = document.createElement("td");
+// var petActionTDButton = document.createElement("button");
+// petActionTDButton.textContent = this.actionText;
+// petActionTD.appendChild(petActionTDButton);
+// petTR.appendChild(petActionTD);
+
+// petActionTDButton.onclick = this.action.bind(this);
+// petsTable.querySelector("tbody").appendChild(petTR)
+// }
+
+class Animal {
+    constructor(name) {
+        this.name = name;
+    }
+
+    action() {
+        document.getElementById(this.actionSoundName).play();
+    }
+
+    putInTheDocument() {
+        var petsTable = document.getElementById("petsTable");
+        var petTR = document.createElement("tr");
+
+        var petNameTD = document.createElement("td");
+        petNameTD.textContent = this.name;
+        petTR.appendChild(petNameTD);
+
+        var petLegsTD = document.createElement("td");
+        petLegsTD.textContent = this.legs;
+        petTR.appendChild(petLegsTD);
+
+        var petActionTD = document.createElement("td");
+        var petActionTDButton = document.createElement("button");
+        petActionTDButton.textContent = this.actionText;
+        petActionTD.appendChild(petActionTDButton);
+        petTR.appendChild(petActionTD);
+
+        petActionTDButton.onclick = this.action.bind(this);
+        petsTable.querySelector("tbody").appendChild(petTR)
+    }
 }
 
-Animal.prototype.action = function(){
-  document.getElementById(this.actionSoundName).play();
-  
+
+// function Cat(name) {
+//     Animal.call(this, name);
+//     this.legs = 4;
+//     this.actionText = "Meoow"
+//     this.actionSoundName = "meow"
+// }
+
+// Cat.prototype = Animal.prototype;
+
+class Cat extends Animal {
+    constructor(name) {
+        super(name);
+        this.legs = 4;
+        this.actionText = "Meoow"
+        this.actionSoundName = "meow"
+    }
 }
 
-Animal.prototype.putInTheDocument = function(){
-    var petsTable = document.getElementById("petsTable");
-    var petTR = document.createElement("tr");
-  
-    var petNameTD = document.createElement("td");
-    petNameTD.textContent = this.name;
-    petTR.appendChild(petNameTD);
-  
-    var petLegsTD = document.createElement("td");
-    petLegsTD.textContent = this.legs;
-    petTR.appendChild(petLegsTD);
-  
-    var petActionTD = document.createElement("td");
-    var petActionTDButton = document.createElement("button");
-    petActionTDButton.textContent = this.actionText;
-    petActionTD.appendChild(petActionTDButton);
-    petTR.appendChild(petActionTD);
-    
-    petActionTDButton.onclick = this.action.bind(this);
-    petsTable.querySelector("tbody").appendChild(petTR)
+// function Monkey(name) {
+//     Animal.call(this, name);
+//     this.legs = 2;
+//     this.actionText = "Scream";
+//     this.actionSoundName = "scream";
+// }
+
+// Monkey.prototype = Animal.prototype;
+
+class Monkey extends Animal {
+    constructor(name) {
+        super(name);
+        this.legs = 4;
+        this.actionText = "Scream"
+        this.actionSoundName = "scream"
+    }
 }
-
-
-function Cat(name){
-    Animal.call(this, name);
-    this.legs = 4;
-    this.actionText = "Meoow"
-    this.actionSoundName = "meow"
-}
-
-Cat.prototype = Animal.prototype;
-
-function Monkey(name){
-    Animal.call(this, name);
-    this.legs = 2;
-    this.actionText = "Scream";
-    this.actionSoundName = "scream";
-}
-
-Monkey.prototype = Animal.prototype;
 
 var Mila = new Cat("Mila");
 Mila.putInTheDocument();
